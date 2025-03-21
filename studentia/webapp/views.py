6d40576a9dd26bb16e6afb9b0d06c34f539af080
AlejandraMiLara <a365544@uabc.edu.mx>
2025-03-21 12:08:59 -0700

    vistas iniciales: login signup sin funcionamiento

--- studentia/webapp/views.py
+++ studentia/webapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def iniciar_sesion(request):
+    return render(request, 'iniciar_sesion.html')
+
+def registrar_usuario(request):
+    return render(request, 'registrar_usuario.html')
